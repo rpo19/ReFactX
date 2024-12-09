9320d10a37b19095dbe0acd8f042362277a7da2b
root <Riccardo Pozzi>
2024-12-09 13:51:26 +0000

    Snapshot before moving to gzip

--- wikidata_downloader.py
+++ wikidata_downloader.py
@@ -1,46 +1,11 @@
-import os
 import requests
 import bz2
-from rdflib import Graph
-import pdb
+import rdflib
 from tqdm import tqdm
+import psycopg
+import sys
 
-ntriple_regex = r'/(<.+>) (<.+>) (.+) \./'
-
-def download_and_decompress(response, chunk_size, decompressor):
-    for chunk in response.iter_content(chunk_size=chunk_size):
-        byte_count = len(chunk)
-        if chunk:
-            g = Graph()
-            decompressed = decompressor.decompress(chunk)
-            # first and last line can ba a problem
-            byte_lines = decompressed.split(b'\n')
-            ok_lines = ''
-
-            for line in byte_lines:
-                line = line.decode()
-                if not line:
-                    # empty line
-                    # print('emtpy')
-                    continue
-                elif line[0] != '<':
-                    # print('no <', line)
-                    # nt line should start with <
-                    continue
-                elif line[-1] != '.':
-                    # print('no .')
-                    # nt line should end with .
-                    # TODO need to continue in next chunk. if last one more
-                    continue
-                else:
-                    ok_lines += line + '\n'
-
-            g.parse(data=ok_lines, format="nt")
-
-            for subject, predicate, object in g:
-                yield 'triple', (subject, predicate, object)
-
-        yield 'byte_count', byte_count
+# python wikidata_downloader.py postgres://postgres:$PASSWORD@postgres:5432/postgres 1 10485760 2>&1 | pcregrep -M -v 'Failed to convert Literal lexical form to value.*(\n|.)*?isodate.isoerror.ISO8601Error.*'
 
 def select_all(sub, pred, obj):
     return True
@@ -48,70 +13,164 @@ def select_all(sub, pred, obj):
 def custom_selector(sub, pred, obj):
     # TODO bisogna estendere :  le label sono rdfs:label
     is_wikidata = sub.startswith('http://www.wikidata.org/entity/Q') and pred.startswith('http://www.wikidata.org/prop/direct/P')
-    # pdb.set_trace()
-    return is_wikidata
+    is_label = str(pred) == 'http://www.w3.org/2000/01/rdf-schema#label' and obj.language == 'en'
+    return is_wikidata or is_label
 
 # def download_and_process(url, output_file, chunk_size=1024*1024, triple_selector=select_all):
-def download_and_process(url, output_file, chunk_size, triple_selector, size=None):
-    # Determine the starting byte
-    start_byte = 0
-    if os.path.exists(output_file):
-        start_byte = os.path.getsize(output_file)
-        print(f"Resuming from byte {start_byte}")
+def download_and_process(url, triple_selector, postgres_connection, worker_num, chunk_size):
+    # postgres://postgres:$POSTGRES_PASSWORD@postgres:5432/postgres
+    with psycopg.connect(postgres_connection, autocommit=False) as conn:
+        # find info from work table
 
-    headers = {"Range": f"bytes={start_byte}-"}
-    # TODO enclose in try except that saves byte_count
-    with requests.get(url, headers=headers, stream=True) as response:
-        # Check if the server supports range requests
-        if response.status_code not in (200, 206):
-            raise Exception(f"Failed to download file: {response.status_code}")
+        with conn.cursor() as cur:
+            cur.execute('SELECT id, bytestart, byteend, lastbyteprocessed, actualbytestart, actualbyteend FROM work WHERE id = %s;', (int(worker_num),))
+            _, bytestart, byteend, lastbyteprocessed, actualbytestart, actualbyteend = cur.fetchone()
 
-        # Open the file in append mode
+        # Determine the starting byte
+        # if os.path.exists(output_file):
+        #     start_byte = os.path.getsize(output_file)
+        #     print(f"Resuming from byte {start_byte}")
 
-        decompressor = bz2.BZ2Decompressor()
+        if lastbyteprocessed > bytestart:
+            bytestart = lastbyteprocessed
+            print('Starting from last processde byte:', bytestart)
 
-        # with open(output_file, "ab") as file:
-        byte_count_cum = 0
-        with tqdm(total=size) as pbar:
-            for key, item in download_and_decompress(response, chunk_size, decompressor):
-                # if chunk:  # Filter out keep-alive chunks
-                #     chunks.append(chunk)
-                #     # decomp = decompressor.decompress(chunk)
-                if key == 'byte_count':
-                    # update count # save it in case of exception
-                    byte_count_cum += item
-                    pbar.update(item)
-                    continue
+        bytestart = 900000
 
-                assert key == 'triple'
+        headers = {"Range": f"bytes={bytestart}-"}
+        # TODO enclose in try except that saves byte_count
+        with requests.get(url, headers=headers, stream=True) as response:
+            # Check if the server supports range requests
+            if response.status_code not in (200, 206):
+                raise Exception(f"Failed to download file: {response.status_code}")
 
-                if not triple_selector(*item):
-                    # skip triple not selected
-                    # print('selector skip', item)
-                    # if 'Q31' in str(item[0]):
-                    #     print(item)
-                    continue
 
-                sub, pred, obj = item
-                # TODO scrivo csv gzippato e parallelizzo
-                sub = str(sub)
-                pred = str(pred)
-                obj = str(obj)
+            decompressor = bz2.BZ2Decompressor()
 
+            # with open(output_file, "ab") as file:
+            byte_count_cum = 0
+
+            with conn.cursor() as cur:
+
+                # with tqdm(total=byteend - bytestart, file=sys.stdout) as pbar:
+
+                staged_line = '' # broken line at the end of the chunk to continue in the next
+
+                relations = []
+                attributes = []
+                labels = []
+
+                stopnext = False
+                for chunk in response.iter_content(chunk_size):
+                    byte_count = len(chunk)
+                    if chunk:
+                        g = rdflib.Graph()
+                        decompressed = decompressor.decompress(chunk)
+                        # first and last line can ba a problem
+                        byte_lines = decompressed.split(b'\n')
+                        ok_lines = ''
+
+                        for line in byte_lines:
+                            line = line.decode()
+                            if not line:
+                                # empty line
+                                # print('emtpy')
+                                continue
+                            elif line[0] != '<':
+                                # print('no <', line)
+                                # nt line should start with <
+                                if staged_line:
+                                    # in this worker
+                                    line = staged_line + line
+                                    staged_line = ''
+                                    assert line[0] == '<' and line[-1] == '.'
+                                else:
+                                    # at byteend start ?
+                                    actualbytestart_new = byte_count + 1
+                                    cur.execute('''UPDATE work SET actualbytestart = %s WHERE id = %s;''', (actualbytestart_new, worker_num))
+                                continue
+                            elif line[-1] != '.':
+                                # print('no .')
+                                # nt line should end with .
+                                # TODO need to continue in next chunk. if last one more
+                                staged_line = line
+                                # TODO go over byteend if there is a stagedline
+                                continue
+
+                            ok_lines += line + '\n'
+                        try:
+                            g.parse(data=ok_lines, format="nt")
+                        except Exception:
+                            assert False
+
+                        selected_triples = (item for item in g if triple_selector(*item))
+
+                        try:
+                            for sub, pred, obj in selected_triples:
+                                save_triple = sub, pred, obj
+                                if str(pred) == 'http://www.w3.org/2000/01/rdf-schema#label':
+                                    # is label
+                                    sub = int(str(sub)[32:])
+                                    obj = str(obj)
+                                    labels.append((sub, obj))
+                                else:
+                                    # is fact
+                                    sub = int(str(sub)[32:])
+                                    pred = int(str(pred)[37:])
+                                    if str(obj).startswith('http://www.wikidata.org/entity/Q'):
+                                        obj = int(str(obj)[32:])
+                                        relations.append((sub, pred, obj))
+                                    # if isinstance(obj, rdflib.term.Literal):
+                                    else:
+                                        # literal
+                                        obj = str(obj)
+                                        attributes.append((sub, pred, obj))
+                        except Exception as e:
+                            print('Exception on triple', save_triple, file=sys.stderr, flush=True)
+                            raise e
+
+                    with cur.copy("COPY relations (subject, predicate, object) FROM STDIN") as copy:
+                        for item in relations:
+                            copy.write_row(item)
+
+                    with cur.copy("COPY attributes (subject, predicate, object) FROM STDIN") as copy:
+                        for item in attributes:
+                            copy.write_row(item)
+
+                    with cur.copy("COPY labels (subject, object) FROM STDIN") as copy:
+                        for item in labels:
+                            copy.write_row(item)
+
+                    byte_count_cum += byte_count
+                    cur.execute('''UPDATE work SET lastbyteprocessed = %s WHERE id = %s;''', (byte_count_cum, worker_num))
+
+                    if bytestart + byte_count_cum > byteend:
+                        cur.execute('''UPDATE work SET actualbyteend = %s WHERE id = %s;''', (byte_count_cum, worker_num))
+
+                    conn.commit()
+
+                    # pbar.update(byte_count)                            # sql copy write
+
+                    if bytestart + byte_count_cum >= byteend:
+                        if stopnext or not staged_line:
+                            # stop
+                            break
+                        else:
+                            # go over byteend for finishing the line
+                            stopnext = True
+                    # sql save chunk
+                    # sql commit
 
     print("Download complete.")
 
-def process_chunk(chunk):
-    # Example processing of the chunk (customize for your needs)
-    print(f"Processing a chunk of size {len(chunk)}")
-
 if __name__ == "__main__":
-    URL = "https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.nt.bz2"
-    OUTPUT_FILE = "latest-all.nt.bz2"
+    URL = "https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.nt.gz"
+
+    postgres_connection = sys.argv[1]
+    worker_num = int(sys.argv[2])
+    chunk_size = int(sys.argv[3])
 
     # try:
-    chunk_size = 1024*1024
-    size = 178101602468
-    download_and_process(URL, OUTPUT_FILE, chunk_size, custom_selector, size)
+    download_and_process(URL, custom_selector, postgres_connection, worker_num, chunk_size)
     # except Exception as e:
         # print(f"Download interrupted: {e}")
