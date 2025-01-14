a2c9f6798a878844af8c0bf4dc851699ef35d392
rpo <pozzir00@gmail.com>
2025-01-14 15:23:17 +0100

    populate postgres support batched childrEN computation

--- populate_postgres.py
+++ populate_postgres.py
@@ -1,7 +1,7 @@
 # CREATE TABLE my_table (
 #     id BIGINT, -- later make it PRIMARY KEY
 #     key BYTEA NOT NULL,
-#     child BYTEA NOT NULL
+#     children BYTEA NOT NULL
 # );
 
 import pickle
@@ -28,13 +28,27 @@ def tkde(bbytes):
     decoded = int.from_bytes(bbytes, byteorder='big', signed=False)
     return decoded
 
-def explode(token_ids, root):
-    key = root
-    for token in token_ids:
-        entoken = tken(token)
-        row = (key, entoken)
-        key += entoken
-        yield row
+def batch_append(trie, token_ids):
+    level = trie
+    for token_id in token_ids:
+        if token_id not in level:
+            level[token_id] = {}
+
+        level = level[token_id]
+
+def get_rows(trie, rootkey):
+    # iterative depth first traversal with a stack
+    key = rootkey
+    stack = [(key, trie)]
+    while len(stack) > 0:
+        key, level = stack.pop()
+        children = level.keys()
+        for child in children:
+            stack.append((key + tken(child), level[child]))
+        if len(children) > 0:
+            # skip adding empty keys to save space
+            yield key, b''.join(map(tken, children))
+
 
 enroot = tken(root)
 
@@ -43,26 +57,26 @@ count = 0
 with psycopg.connect(postgres_connection, autocommit=False) as conn:
     with conn.cursor() as cur:
         cur.execute("TRUNCATE TABLE ctrie;")
-        with cur.copy("COPY ctrie (key, child) FROM STDIN WITH (FREEZE)") as copy:
+        with cur.copy("COPY ctrie (key, children) FROM STDIN WITH (FREEZE)") as copy:
             with bz2.BZ2File(fname, "rb") as bz2file:
                 with tqdm(total=total_rows) as pbar:
-                    batch = set()
+                    batch = {}
 
                     while True:
                         try:
 
                             # Load each pickled object from the bz2 file
                             array = pickle.load(bz2file)
-                            batch = batch.union(set(explode(array, enroot)))
+                            batch_append(batch, array)
 
                             count += 1
 
                             if count % batch_size == 0:
                                 # batch on number or rows processed
-                                for row in batch:
+                                for row in get_rows(batch):
                                     copy.write_row(row)
                                 # reset batch
-                                batch = set()
+                                batch = {}
 
                             if count % 10000 == 0:
                                 pbar.n = count
