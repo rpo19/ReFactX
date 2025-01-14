30243c3f0bce5e4f9ba0ec00723de72a5b52a157
rpo <pozzir00@gmail.com>
2025-01-14 14:43:03 +0100

    add batch processing to postgres population

--- populate_postgres.py
+++ populate_postgres.py
@@ -1,8 +1,7 @@
 # CREATE TABLE my_table (
 #     id BIGINT, -- later make it PRIMARY KEY
 #     key BYTEA NOT NULL,
-#     child BYTEA NOT NULL,
-#     child_id BIGINT NOT NULL
+#     child BYTEA NOT NULL
 # );
 
 import pickle
@@ -16,7 +15,8 @@ import itertools
 fname = sys.argv[1]
 postgres_connection = sys.argv[2] # 'postgres://postgres:secret@host:5432/postgres'
 root = int(sys.argv[3]) # number that not collides with the vocab (check max vocab) # maybe 60000
-total_rows = int(sys.argv[4]) if len(sys.argv) > 4 else None
+batch_size = int(sys.argv[4])
+total_rows = int(sys.argv[5]) if len(sys.argv) > 5 else None
 
 def tken(token):
     # token encode
@@ -28,32 +28,39 @@ def tkde(bbytes):
     decoded = int.from_bytes(bbytes, byteorder='big', signed=False)
     return decoded
 
-def explode(token_ids, root, peekable_id):
+def explode(token_ids, root):
     key = root
     for token in token_ids:
         entoken = tken(token)
-        row = (next(peekable_id), key, entoken, peekable_id.peek())
+        row = (key, entoken)
         key += entoken
         yield row
 
 enroot = tken(root)
-
-peekable_id = peekable(itertools.count())
 
 count = 0
 
 with psycopg.connect(postgres_connection, autocommit=False) as conn:
     with conn.cursor() as cur:
         cur.execute("TRUNCATE TABLE ctrie;")
-        with cur.copy("COPY ctrie (id, key, child, child_id) FROM STDIN WITH (FREEZE)") as copy:
+        with cur.copy("COPY ctrie (key, child) FROM STDIN WITH (FREEZE)") as copy:
             with bz2.BZ2File(fname, "rb") as bz2file:
                 with tqdm(total=total_rows) as pbar:
+                    batch = set()
+
                     while True:
                         try:
+
                             # Load each pickled object from the bz2 file
                             array = pickle.load(bz2file)
-                            for row in explode(array, enroot, peekable_id):
-                                copy.write_row(row)
+                            batch = batch.union(set(explode(array, enroot)))
+
+
+                            if len(batch) >= batch_size:
+                                for row in batch:
+                                    copy.write_row(row)
+                                # reset batch
+                                batch = set()
 
                             count += 1
 
