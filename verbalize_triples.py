4ef0be3ec3e4ce72a6e10e8b1a259f6389ca16e7
Riccardo Pozzi <pozzir00@gmail.com>
2025-01-20 21:22:12 +0000

    Update verbalize triples

--- verbalize_triples.py
+++ verbalize_triples.py
@@ -18,23 +18,25 @@ import gzip
 import json
 import bz2
 import re
+import sys
 
-with gzip.open('/workspace/data/filtered_props_dict.gz', 'r') as fd:
+props_mapping = sys.argv[1] # '/workspace/data/filtered_props_dict.gz'
+entity_mapping = sys.argv[2] # '/workspace/data/wikidata_titles_mapping.pickle'
+wiki_dump = sys.argv[3] # '/workspace/data/latest-truthy.nt.bz2'
+outfile = sys.argv[4] # '/workspace/data/verbalized.latest-truthy.nt.bz2'
+total_number_of_triples = sys.argv[5] if len(sys.argv) > 5 else None
+
+with gzip.open(props_mapping, 'r') as fd:
     prop_labels = json.load(fd)
 
-with open('/workspace/data/wikidata_titles_mapping.pickle', 'rb') as fd:
+with open(entity_mapping, 'rb') as fd:
     ent_labels = pickle.load(fd)
-
-wiki_dump = '/workspace/data/latest-truthy.nt.bz2'
-
-outfile = '/workspace/data/verbalized.latest-truthy.nt.bz2'
 
 triple_regex = re.compile(r'<http:\/\/www\.wikidata\.org\/entity\/Q([0-9]+)>\s+'
 r'<(?:http:\/\/www\.wikidata\.org\/prop\/direct\/P([0-9]+)|http:\/\/schema\.org\/description)>\s+' # added schema description # todo test
 r'(?:<http:\/\/www\.wikidata\.org\/entity\/Q([0-9]+)>|"([^"]+)"@en)\s+\.')
 
-tqdm_params = {'total': 7923563616}
-#tqdm_params = {}
+tqdm_params = {'total': total_number_of_triples}
 
 added_shortdesc = set()
 
@@ -47,42 +49,43 @@ with tqdm(**tqdm_params) as pbar:
                 if match:
                     sub, prop, obj_ent, obj_lit = match.groups()
 
-                    sub_id = int(sub)
-                    v_sub = ent_labels.get(sub_id, {}).get('title')
+                    if sub.isnumeric():
+                        sub_id = int(sub)
+                        v_sub = ent_labels.get(sub_id, {}).get('title')
 
-                    # short description for subject entity
-                    sub_short_desc = ent_labels.get(sub_id, {}).get('short_desc')
-                    if sub_id not in added_shortdesc and sub_short_desc:
-                        added_shortdesc.add(sub_id)
-                        outfd.write(f'<{v_sub}> <short description> <{sub_short_desc}> .\n'.encode('utf-8'))
+                        # short description for subject entity
+                        sub_short_desc = ent_labels.get(sub_id, {}).get('short_desc')
+                        if sub_id not in added_shortdesc and sub_short_desc:
+                            added_shortdesc.add(sub_id)
+                            outfd.write(f'<{v_sub}> <short description> <{sub_short_desc}> .\n'.encode('utf-8'))
 
-                    if prop is None:
-                        # description
-                        assert obj_lit is not None
-                        v_prop = 'description'
-                        v_obj = obj_lit
+                        if v_sub and prop is None:
+                            # description
+                            assert obj_lit is not None
+                            v_prop = 'description'
+                            v_obj = obj_lit
 
-                        outfd.write(f'<{v_sub}> <{v_prop}> <{v_obj}> .\n'.encode('utf-8'))
-                    else:
-                        v_prop = prop_labels.get('P'+prop, {}).get('label')
-                        if v_sub and v_prop:
-                            if obj_ent:
-                                # obj is entity
-                                obj_id = int(obj_ent)
-                                v_obj = ent_labels.get(obj_id, {}).get('title')
+                            outfd.write(f'<{v_sub}> <{v_prop}> <{v_obj}> .\n'.encode('utf-8'))
+                        else:
+                            v_prop = prop_labels.get('P'+prop, {}).get('label')
+                            if v_sub and v_prop:
+                                if obj_ent and obj_ent.isnumeric()::
+                                    # obj is entity
+                                    obj_id = int(obj_ent)
+                                    v_obj = ent_labels.get(obj_id, {}).get('title')
 
-                                # short description for object entity
-                                obj_short_desc = ent_labels.get(obj_id, {}).get('short_desc')
-                                if obj_id not in added_shortdesc and obj_short_desc:
-                                    added_shortdesc.add(obj_id)
-                                    outfd.write(f'<{v_obj}> <short description> <{obj_short_desc}> .\n'.encode('utf-8'))
-                            if obj_lit:
-                                # obj is literal
-                                v_obj = obj_lit
+                                    # short description for object entity
+                                    obj_short_desc = ent_labels.get(obj_id, {}).get('short_desc')
+                                    if obj_id not in added_shortdesc and obj_short_desc:
+                                        added_shortdesc.add(obj_id)
+                                        outfd.write(f'<{v_obj}> <short description> <{obj_short_desc}> .\n'.encode('utf-8'))
+                                if obj_lit:
+                                    # obj is literal
+                                    v_obj = obj_lit
 
-                            if v_obj:
-                                # verify v_obj is not None or ''
-                                outfd.write(f'<{v_sub}> <{v_prop}> <{v_obj}> .\n'.encode('utf-8'))
+                                if v_obj:
+                                    # verify v_obj is not None or ''
+                                    outfd.write(f'<{v_sub}> <{v_prop}> <{v_obj}> .\n'.encode('utf-8'))
 
                     # else:
                     #     # cannot find v_sub or v_prop --> skip
