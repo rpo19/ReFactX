68c45a099ba003d24f53f84bbede1b214591d411
root <Riccardo Pozzi>
2025-03-06 17:13:53 +0000

    Merge tokenize_triples in populate_postgres

--- populate_postgres.py
+++ populate_postgres.py
@@ -1,31 +1,32 @@
-import pickle
 import bz2
 import psycopg
 from tqdm import tqdm
-import pickle
 from ctrie import PostgresIngestIndex
+from transformers import AutoTokenizer
 import click
 
 @click.command()
 @click.argument('fname', type=click.Path(exists=True))
+@click.option("--model-name", required=True, help="Name of the model to use.")
 @click.option('--postgres-connection', type=str, required=True, help='PostgreSQL connection string')
 @click.option('--table-name', type=str, required=True, help='Database table name')
+@click.option("--prefix", required=True, help="Prefix used in processing.")
+@click.option("--end-of-triple", required=True, help="End of triple marker.")
 @click.option('--rootkey', type=int, required=True, help='Root key value')
-@click.option('--batch-size', type=int, required=True, help='Batch size for processing')
+@click.option("--tokenizer-batch-size", type=int, required=True, help="Batch size for tokenizer.")
+@click.option('--batch-size', type=int, required=True, help='Batch size for ingestion (must be multiple of --tokenizer-batch-size).')
 @click.option('--switch-parameter', type=int, required=True, help='Switch parameter value')
-@click.option('--total-rows', type=int, required=False, help='Total number of rows (optional)')
-def main(fname, postgres_connection, table_name, rootkey, batch_size, switch_parameter, total_rows):
+@click.option("--total-number-of-triples", type=int, default=None, help="Total number of items (optional).")
+@click.option("--count-leaves", is_flag=True, default=False, help="Count leaves to veify the count is correct (slower).")
+def main(fname, model_name, postgres_connection, table_name, prefix, end_of_triple, rootkey,
+    tokenizer_batch_size, batch_size, switch_parameter, total_number_of_triples, count_leaves):
     """Command-line tool for processing data and storing it in a PostgreSQL database."""
-    click.echo(f"Processing file: {fname}")
-    click.echo(f"Database connection: {postgres_connection}")
-    click.echo(f"Table name: {table_name}")
-    click.echo(f"Root key: {rootkey}")
-    click.echo(f"Batch size: {batch_size}")
-    click.echo(f"Switch parameter: {switch_parameter}")
-    if total_rows is not None:
-        click.echo(f"Total rows: {total_rows}")
-    else:
-        click.echo("Total rows: Not provided")
+
+    assert batch_size % tokenizer_batch_size == 0, f'ERROR: --batch-size ({batch_size}) must be multiple of --tokenizer-batch-size ({tokenizer_batch_size})'
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    if not tokenizer.is_fast:
+        print('WARNING: tokenizer is not fast.')
 
     index = PostgresIngestIndex(
                 rootkey=rootkey,
@@ -36,7 +37,7 @@ def main(fname, postgres_connection, table_name, rootkey, batch_size, switch_par
         for sequence in nested_token_ids:
             index.add(sequence)
 
-    tbar_update = batch_size
+    tbar_update = tokenizer_batch_size
     count = 0
 
     with psycopg.connect(postgres_connection, autocommit=False) as conn:
@@ -54,22 +55,45 @@ def main(fname, postgres_connection, table_name, rootkey, batch_size, switch_par
 
             cur.execute(index.truncate_query)
             with cur.copy(index.copy_query) as copy:
-                with bz2.BZ2File(fname, "rb") as bz2file:
-                    with tqdm(total=total_rows) as pbar:
-                        while True:
+                with bz2.BZ2File(fname) as bz2file:
+                    with tqdm(total=total_number_of_triples) as pbar:
+                        tokenizer_batch = []
+                        for count, bline in enumerate(bz2file):
                             try:
-                                # Load each pickled object from the bz2 file
-                                array = pickle.load(bz2file)
-                                batch_append(array, index)
+                                line = bline.decode()
+                                if line[-1] == '\n':
+                                    line = line[:-1]
 
-                                count += 1
+                                if not line.endswith(end_of_triple):
+                                    print(f'WARNING: line ({line}) w/o --end-of-triple ({end_of_triple})')
+                                    line = line + end_of_triple
 
-                                if count % batch_size == 0:
+                                line = prefix + line
+
+                                tokenizer_batch.append(line)
+
+                                if len(tokenizer_batch) == tokenizer_batch_size:
+                                    ids = tokenizer(tokenizer_batch)['input_ids']
+                                    tokenizer_batch = []
+
+                                    batch_append(ids, index)
+
+                                if count % batch_size == 0 and count > 0:
                                     # batch on number or rows processed
                                     for row in index.get_rows():
                                         copy.write_row(row)
+
+                                    if count_leaves:
+                                        try:
+                                            lfc = index.count_leaves(fail_on_wrong_num=True)
+                                        except WrongNumleavesException as e:
+                                            print(e, 'at', count)
+
                                     # reset batch
                                     index.reset()
+
+                                    # debug
+                                    break
 
                                 if count % tbar_update == 0:
                                     pbar.n = count
