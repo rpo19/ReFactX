efc9ffaf52b66ab0841b1137a04dccbe6dd5fa59
root <Riccardo Pozzi>
2025-03-12 15:41:47 +0000

    Add 2wikimultihop dataset config

---
+++ 2wikimultihop_sample_dev.py
@@ -0,0 +1,19 @@
+import random
+import json
+from base_dataset_config import QADataset
+
+config = {
+    'path': '/workspace/data/2wikimultihop/data/dev.json',
+    'random_seed': 1234,
+    'num_samples': 50,
+}
+
+print(f'Loading {config["path"]}')
+with open(config['path']) as fd:
+    raw_dataset = json.load(fd)
+
+# sample
+random.seed(config['random_seed'])
+raw_dataset = random.sample(raw_dataset, config['num_samples'])
+
+dataset = QADataset(raw_dataset, config)
