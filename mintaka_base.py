7c63edf80e4e30b6ee90a2fe9fb9f074e84af279
Riccardo Pozzi <pozzir00@gmail.com>
2025-03-24 14:32:53 +0100

    Fix mintaka base

--- mintaka_base.py
+++ mintaka_base.py
@@ -2,8 +2,8 @@ import json
 from base_dataset_config import QADataset
 
 class MintakaDataset(QADataset):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def get_answer(self, i):
         return self.dataset[i]['answer']['mention']
