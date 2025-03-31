393b0f60840cc317a5067cb930e776c171acef58
Riccardo Pozzi <pozzir00@gmail.com>
2025-03-31 22:29:08 +0200

    Add DATASET_START_FROM to continue experiments
    In case of failures or partial excecution you can quickly continue from
    the element i of the dataset. Avoiding to repeat already-done
    operations.

--- mintaka_test.py
+++ mintaka_test.py
@@ -1,4 +1,5 @@
 import json
+import os
 from mintaka_base import MintakaDataset
 
 path = MintakaDataset.get_dataset_path('mintaka_test.json')
@@ -11,4 +12,13 @@ print(f'Loading {config["path"]}')
 with open(config['path']) as fd:
     raw_dataset = json.load(fd)
 
+DATASET_START_FROM = int(os.environ.get('DATASET_START_FROM', 0))
+if DATASET_START_FROM:
+    raw_dataset = raw_dataset[DATASET_START_FROM:]
+
 dataset = MintakaDataset(raw_dataset, config)
+
+if __name__ == '__main__':
+    print('Length', len(dataset))
+    print('First', dataset[0])
+ 
