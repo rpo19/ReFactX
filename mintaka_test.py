4a2a935be1121b9d01d636e971a4b6953d29bcf1
rpo <pozzir00@gmail.com>
2025-04-01 16:44:20 +0200

    Fix dataset start from

--- mintaka_test.py
+++ mintaka_test.py
@@ -15,6 +15,7 @@ with open(config['path']) as fd:
 DATASET_START_FROM = int(os.environ.get('DATASET_START_FROM', 0))
 if DATASET_START_FROM:
     raw_dataset = raw_dataset[DATASET_START_FROM:]
+    config['start_from'] = DATASET_START_FROM
 
 dataset = MintakaDataset(raw_dataset, config)
 
