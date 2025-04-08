38161321a1a05da11a7c1f788af37247e5a2087b
rpo <pozzir00@gmail.com>
2025-04-08 15:24:25 +0200

    [WIP] adapting textgrad

---
+++ mintaka_dev_ssample72.py
@@ -0,0 +1,29 @@
+import json
+import os
+from mintaka_base import MintakaDataset
+
+path = MintakaDataset.get_dataset_path('mintaka_dev_ssample72.json')
+
+config = {
+    'path': path,
+    'random_seed': 42,
+    'sample_size': 72,
+    'stratify': True,
+    'columns': ['complexityType', 'category'],
+}
+
+print(f'Loading {config["path"]}')
+with open(config['path']) as fd:
+    raw_dataset = json.load(fd)
+
+DATASET_START_FROM = int(os.environ.get('DATASET_START_FROM', 0))
+if DATASET_START_FROM:
+    raw_dataset = raw_dataset[DATASET_START_FROM:]
+    config['start_from'] = DATASET_START_FROM
+
+dataset = MintakaDataset(raw_dataset, config)
+
+if __name__ == '__main__':
+    print('Length', len(dataset))
+    print('First', dataset[0])
+
---
+++ mintaka_train.py
@@ -0,0 +1,25 @@
+import json
+import os
+from mintaka_base import MintakaDataset
+
+path = MintakaDataset.get_dataset_path('mintaka_train.json')
+
+config = {
+    'path': path
+}
+
+print(f'Loading {config["path"]}')
+with open(config['path']) as fd:
+    raw_dataset = json.load(fd)
+
+DATASET_START_FROM = int(os.environ.get('DATASET_START_FROM', 0))
+if DATASET_START_FROM:
+    raw_dataset = raw_dataset[DATASET_START_FROM:]
+    config['start_from'] = DATASET_START_FROM
+
+dataset = MintakaDataset(raw_dataset, config)
+
+if __name__ == '__main__':
+    print('Length', len(dataset))
+    print('First', dataset[0])
+
