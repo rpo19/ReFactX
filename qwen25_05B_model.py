b0411b1fdbe6ccb6256cf2bf7069ade4fc338ac8
rpo <pozzir00@gmail.com>
2025-04-13 16:52:47 +0200

    Add qwen 0.5

---
+++ qwen25_05B_model.py
@@ -0,0 +1,7 @@
+from base_model_config import ModelConfig
+
+model_config = ModelConfig(
+    model_name = 'Qwen/Qwen2.5-0.5B-Instruct',
+    switch_pattern = [17417, 25], # "Fact:" after newline
+    newline_token = 198,
+)
