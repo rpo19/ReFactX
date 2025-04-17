3420ca78711422312d44ccde9cec239900d34c8a
rpo <pozzir00@gmail.com>
2025-04-17 11:20:28 +0200

    Improve BI dataset
    Translate the relations in a more natural form. Add inverse relations
    and reflexive ones.
    
    Modify BI prompt so that it assumes that no fact means the fact is
    false and to avoid using descriptions (not present in this KB).
    
    Move prompt template in dataset config to easier modify prompts for
    different datasets.

--- bi_base.py
+++ bi_base.py
@@ -3,6 +3,115 @@ from zipfile import ZipFile
 import os
 import pandas as pd
 
+# TODO this dataset assumes it is complete: if there is no evidence that proves something, then it is false.
+
+# different prompt
+# there are no description and short description
+BI_BASE_PROMPT_TEMPLATE = [
+    {
+        'role': 'system',
+        'content': '''You are a helpful question-answering assistant that bases its answers on facts from a knowledge base and always respects the prompt.
+
+Process:
+
+    You receive an input question.
+
+    You determine the reasoning path needed to answer the question based on the information available.
+
+    You explicitly list relevant facts, one per line, starting with "Fact:".
+
+    You explain your reasoning step by step and provide a detailed answer, justifying it based on the facts.
+
+    You conclude with a short and concise answer that must be grounded by the facts you found.
+
+This knowledge base is complete. If a fact is not present we assume that facts is false.
+
+After generating facts, please reason on top of the facts you find and avoid generating lot of useless facts.
+
+You must always follow these instructions precisely and ensure your responses adhere strictly to this prompt.'''
+    },
+            {
+                'role': 'user',
+                'content': '''Which mountain is taller between Mont Blanc and Mount Rainier?
+'''
+            },
+            {
+                'role': 'assistant',
+                'content': '''Reasoning: I need to provide the height of Mont Blanc and the height of Mount Rainier, then I need to compare the two heights and the final answer will be the taller mountain.
+Fact: <Mont Blanc> <elevation above sea level> <4,807.02±0.5 meters> .
+Now I need the height of Mount Rainier.
+Fact: <Mount Rainier> <elevation above sea level> <4,389 meters> .
+Long answer: Mont Blanc is 4,807 meters tall, while Mount Rainier is 4,389 meters, so Mont Blanc is taller than Mount Rainier.
+
+Answer: Mont Blanc.
+'''
+            },
+            {
+                'role': 'user',
+                'content': '''When was the director of Slumdog Millionaire born?
+'''
+            },
+            {
+                'role': 'assistant',
+                'content': '''Reasoning: To answer this question, I need to find the birth date of the director of Slumdog Millionaire.
+Fact: <Slumdog Millionaire> <director> <Danny Boyle>.
+Now I need the birth date of Danny Boyle.
+Fact: <Danny Boyle> <date of birth> <1956-10-20T00:00:00Z>.
+Long answer: The director of Slumdog Millionaire is Danny Boyle, who was born on October 20, 1956, so this is the answer to the question.
+
+Answer: 20-Oct-56.
+'''
+            },
+            {
+                'role': 'user',
+                'content': '''When did John V, Prince Of Anhalt-Zerbst's father die?
+'''
+            },
+            {
+                'role': 'assistant',
+                'content': '''Reasoning: To answer this question, I need to find the date of death of John V, Prince of Anhalt-Zerbst's father.
+Fact: <John V, Prince of Anhalt-Zerbst> <father> <Ernest I, Prince of Anhalt-Dessau> .
+Fact: <Ernest I, Prince of Anhalt-Dessau> <date of death> <1516-06-12T00:00:00Z> .
+Long answer: The father of John V, Prince of Anhalt-Zerbst, Ernest I, Prince of Anhalt-Dessau, died on June 12, 1516.
+
+Answer: June 12, 1516.
+'''
+            },
+            {
+                'role': 'user',
+                'content': '''Is Johnny Depp older than Brad Pitt?
+'''
+            },
+            {
+                'role': 'assistant',
+                'content': '''Reasoning: To determine if Johnny Depp is older than Brad Pitt, I need to find their respective birth years.
+Fact: <Johnny Depp> <date of birth> <1963-06-09T00:00:00Z> .
+Fact: <Brad Pitt> <date of birth> <1963-12-18T00:00:00Z> .
+Long answer: Johnny Depp was born on June 9, 1963, while Brad Pitt was born on December 18, 1963. So yes, Johnny Depp is older than Brad Pitt.
+
+Answer: Yes.
+'''
+    },
+    {
+        'role': 'user',
+        'content': '''Has Hillary Clinton ever served as President of the United States?'''
+    },
+    {
+        'role': 'assistant',
+        'content': '''Reasoning: To answer this question, I need to find out if Hillary Clinton has ever held the position of President of the United States.
+Fact: <Hillary Clinton> <position held> <United States Secretary of State> .
+This fact tells me that Hillary Clinton was the United States Secretary of State, but it doesn't answer the question about her presidency.
+Fact: <Hillary Clinton> <occupation> <Politician> .
+This facts tells she is a Politician but it is not enough.
+Fact: <Hillary Clinton> <occupation> <Diplomat> .
+This facts tells she is a Diplomat but not that she served as President.
+I don't have any facts that directly answer the question about Hillary Clinton's presidency, so I assume Hillary Clinton never served as President of the United States.
+Long answer: Since there are no facts confirming that Hillary Clinton served as President of the United States, I assume she never did it, so the answer is no.
+
+Answer: No.'''
+    },
+]
+
 class BIDataset(QADataset):
     def load_from_path(self, dataset_path):
         bi_data_path2 = os.environ.get('BI_DATA_PATH2')
--- bi_index.py
+++ bi_index.py
@@ -9,6 +9,9 @@ from transformers import AutoTokenizer
 class IndexConfigException(Exception):
     pass
 
+# TODO should I add preds in the prompt?
+# 'REACHABLE', 'OWN', 'ROLE', 'CONTROL', 'ULTIMATE_CONTROL', 'HAS_HOLDINGS'
+# ask to modify template to more real-world questions: no entities but "which person or company is connected to...?"
 class IndexConfig():
     def __init__(self, model_name, end_of_triple_str=" .", prefix=" ", batch_size=100, add_special_tokens=False):
         self.model_name = model_name # the db depends from the tokenizer
@@ -24,7 +27,30 @@ class IndexConfig():
         self.batch_size = batch_size
         self.add_special_tokens = add_special_tokens
 
-        self.skip_serialize = set(['skip_serialize', 'index', 'tokenizer'])
+        # TODO explain them in a prompt? # experiment both with and without
+        self.relmapping = {
+            'role': dict(mappings=['has role in']),
+            'own': dict(mappings=['owns'], inverse=['owned by']),
+            'control': dict(mappings=['controls'], inverse=['controlled by']),
+            'ultimate_control': dict(mappings=['ultimately controls', 'ultimate controller of', 'controls ultimately'], inverse=['ultimately controlled by', 'controlled ultimately by']),
+            'has_holdings': dict(mappings=['has holdings in']),
+            'reachable': dict(mappings=['connected to', 'reachable from'], reflexive=True),
+        }
+
+        self.skip_serialize = set(['skip_serialize', 'index', 'tokenizer', 'raw_triples', 'verbalized_triples'])
+
+    def enrich_relations(self):
+        for triple in self.raw_triples:
+            mappings = self.relmapping[triple['relation']]
+            assert isinstance(mappings['mappings'], list)
+            for mapping in mappings['mappings']:
+                yield {'head': triple['head'], 'tail': triple['tail'], 'relation': mapping}
+                if mappings.get('reflexive'):
+                    yield {'head': triple['tail'], 'tail': triple['head'], 'relation': mapping}
+            inverse_mappings = mappings.get('inverse', [])
+            assert isinstance(inverse_mappings, list)
+            for inverse_mapping in inverse_mappings:
+                yield {'head': triple['tail'], 'tail': triple['head'], 'relation': inverse_mapping}
 
     def load_from_path(self, dataset_path):
         actual_path = QADataset.get_dataset_path(dataset_path)
@@ -35,20 +61,28 @@ class IndexConfig():
             with myzip.open('anony_triples_v0.csv', pwd=bi_data_path2.encode()) as myfile:
                 self.raw_triples = pd.read_csv(myfile)
 
+        # lowercase relations
+        self.raw_triples['relation'] = self.raw_triples['relation'].apply(lambda x: x.lower())
+        self.raw_triples = self.raw_triples.to_dict(orient='records')
+        self.raw_triples = list(self.enrich_relations())
         self.transform_triples()
-        self.tokenize_triples()
-        
+        self.tokenize_triples()        
 
     def format_triple(self, triple):
+        head = triple['head']
+        tail = triple['tail']
+        relation = triple['relation']
         verb = '{prefix}<{head}> <{relation}> <{tail}>{end_of_triple}'.format(
             prefix=self.prefix,
             end_of_triple=self.end_of_triple_str,
-            **triple
+            head=head,
+            tail=tail,
+            relation=relation,
             )
         return verb
 
     def transform_triples(self):
-        self.verbalized_triples = list(map(self.format_triple, self.raw_triples.to_dict(orient='records')))
+        self.verbalized_triples = list(map(self.format_triple, self.raw_triples))
 
     def batch_append(self, nested_token_ids):
         for sequence in nested_token_ids:
