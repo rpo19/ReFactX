7ad0cde0e1973bb71e58a084ba8f4fae8c73cce1
root <Riccardo Pozzi>
2025-05-01 21:13:18 +0000

    Add qwen3 4B

--- qwen25_3B_model.py
+++ qwen25_3B_model.py
@@ -2,4 +2,6 @@ from qwen25_model_config import QwenModelConfig
 
 model_config = QwenModelConfig(
     model_name = 'Qwen/Qwen2.5-3B-Instruct',
+    #device_map='auto',
+    # torch_dtype='bfloat16'
 )
