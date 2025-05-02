7ce1555089ea11b8fdce66c18a4acab602795810
Riccardo Pozzi <pozzir00@gmail.com>
2025-05-02 11:07:56 +0200

    Track hpc changes

--- llama_70B_model.py
+++ llama_70B_model.py
@@ -2,5 +2,6 @@ from llama_model_config import LlamaModelConfig
 
 model_config = LlamaModelConfig(
     model_name = 'meta-llama/Llama-3.3-70B-Instruct',
+    device_map='auto',
 )
 model_config.batch_size=4
--- qwen25_05B_model.py
+++ qwen25_05B_model.py
@@ -3,4 +3,5 @@ from qwen25_model_config import QwenModelConfig
 
 model_config = QwenModelConfig(
     model_name = 'Qwen/Qwen2.5-0.5B-Instruct',
+    device_map = 'auto',
 )
