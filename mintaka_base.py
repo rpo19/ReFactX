336335fbbb145cd8a4904f0b3b99bfe73b79e28e
Riccardo Pozzi <pozzir00@gmail.com>
2025-05-04 23:55:41 +0200

    Fix analyze and mintaka

--- mintaka_base.py
+++ mintaka_base.py
@@ -5,7 +5,7 @@ class MintakaDataset(QADataset):
         return sample['answer']['mention']
     def get_answer_type(self, sample):
         answer_type = sample['answer']['answerType']
-        if len(sample['answer']['answer']) > 1:
+        if sample['answer']['answer'] and len(sample['answer']['answer']) > 1:
             answer_type += ',list'
         return answer_type
     def get_question_type(self, sample):
