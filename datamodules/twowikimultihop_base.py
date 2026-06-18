1dffa5908dc6185d6084109f91bbb80aa6326913
root <pozzir00@gmail.com>
2026-06-18 15:26:48 +0000

    Remove deprecated datamodules directory

--- datamodules/twowikimultihop_base.py
+++
@@ -1,6 +0,0 @@
-from base_dataset_config import QADataset
-
-class TwoWikiMultiHopDataset(QADataset):
-    def get_question_type(self, sample):
-        # Optionally, extract the type of question (e.g., single-hop or multi-hop)
-        return sample['type']
